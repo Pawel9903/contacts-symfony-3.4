813234f18ac4c77b742a629ada59c63f93efab5a
Pawel9903 <pawel9903@wp.pl>
2018-02-22 14:43:29 +0100

    Dodanie FosUser bundle

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pawel
+ * Date: 22.02.2018
+ * Time: 14:18
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+}
