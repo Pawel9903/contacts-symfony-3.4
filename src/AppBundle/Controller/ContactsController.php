153dd2ac74aaf3f9c30e2c68165ecf04be95c0e7
Pawel9903 <pawel9903@wp.pl>
2018-03-16 09:38:59 +0100

    dodanie MyContactsController i zmiana routingów

--- src/AppBundle/Controller/ContactsController.php
+++ src/AppBundle/Controller/ContactsController.php
@@ -21,42 +21,10 @@ class ContactsController extends Controller
         return $this->render("Contacts/index.html.twig");
     }
 
-    public function myContactsAction()
-    {
-        $entityManager = $this->getDoctrine()->getManager();
-        $contacts = $entityManager->getRepository(Contacts::class)->findAll();
-
-        return $this->render("MyContacts/index.html.twig", ['contacts'=>$contacts]);
-    }
 
     public function detailsAction(Contacts $contact)
     {
         return $this->render('Contacts/details.html.twig',['contact'=>$contact]);
     }
 
-    public function addAction(Request $request)
-    {
-        $contact = new Contacts();
-        $form = $this->createForm(ContactType::class, $contact);
-
-        if($request->isMethod('post'))
-        {
-            $form->handleRequest($request);
-
-            if($form->isValid())
-            {
-                $entityManager = $this->getDoctrine()->getManager();
-                $entityManager->persist($contact);
-                $entityManager->flush();
-
-                $this->addFlash("success", "Contact {$contact->getName()} has been successfully added");
-
-                return $this->redirectToRoute('contacts_details',['id'=>$contact->getId()]);
-            }
-                $this->addFlash("error","Add Failed");
-        }
-
-        return $this->render('MyContacts/add.html.twig',['form'=>$form->createView()]);
-
-    }
 }
---
+++ src/AppBundle/Controller/MyContactsController.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pawel
+ * Date: 16.03.2018
+ * Time: 09:27
+ */
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Contacts;
+use AppBundle\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class MyContactsController extends Controller
+{
+    public function contactsAction()
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $contacts = $entityManager->getRepository(Contacts::class)->findAll();
+
+        return $this->render("MyContacts/contacts.html.twig", ['contacts'=>$contacts]);
+    }
+
+    public function detailsAction(Contacts $contact)
+    {
+        return $this->render('MyContacts/details.html.twig',['contact'=>$contact]);
+    }
+
+    public function addAction(Request $request)
+    {
+        $contact = new Contacts();
+        $form = $this->createForm(ContactType::class, $contact);
+
+        if($request->isMethod('post'))
+        {
+            $form->handleRequest($request);
+
+            if($form->isValid())
+            {
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($contact);
+                $entityManager->flush();
+
+                $this->addFlash("success", "Contact {$contact->getName()} has been successfully added");
+
+                return $this->redirectToRoute('contacts_details',['id'=>$contact->getId()]);
+            }
+            $this->addFlash("error","Add Failed");
+        }
+
+        return $this->render('MyContacts/add.html.twig',['form'=>$form->createView()]);
+
+    }
+
+}
